df4202c1a385992ecbbbb94cb2cdb1147a4ac9f1
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-23 18:03:37 +0530

    💬 comments

--- clickgen/x11.py
+++ clickgen/x11.py
@@ -1,3 +1,7 @@
+"""
+    xcursorgen.c python api
+"""
+
 import ctypes
 import os
 
@@ -6,6 +10,7 @@ libpath = os.path.join(basedir, 'xcursorgen.so')
 
 dll = ctypes.CDLL(libpath)
 
+# main function ctypes define
 LP_c_char = ctypes.POINTER(ctypes.c_char)
 LP_LP_c_char = ctypes.POINTER(LP_c_char)
 
@@ -15,9 +20,11 @@ dll.main.restypes = ctypes.c_int
 
 def __gen_argv_ctypes(argv: list):
     p = (LP_c_char*len(argv))()
+
     for i, arg in enumerate(argv):  # not sys.argv, but argv!!!
         enc_arg = arg.encode('utf-8')
         p[i] = ctypes.create_string_buffer(enc_arg)
+
     return ctypes.cast(p, LP_LP_c_char)
 
 
