1919d656b3dcca4e2fc221890ada8b455975216e
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-25 19:25:21 +0530

    🚨 Error Handling

--- clickgen/utils.py
+++ clickgen/utils.py
@@ -1,6 +1,22 @@
+import glob
 import os
+
 from clickgen.x11 import main as x11_gen
 from clickgen.win import main as win_gen
+
+config_ext = ('*.in', '*.ini')
+
+
+def have_configs(dir: str) -> bool:
+    files_grabbed = []
+
+    for files in config_ext:
+        files_grabbed.extend(glob.glob(files))
+
+    if len(files_grabbed) >= 0:
+        return True
+    else:
+        return False
 
 
 def main(config_dir: str,
@@ -19,4 +35,9 @@ def main(config_dir: str,
     in_path = os.path.abspath(config_dir)
     out_path = os.path.abspath(out_path)
 
-    x11_gen(in_path, out_path)
+    try:
+        flag = have_configs(config_dir)
+        if (flag == True):
+            raise FileNotFoundError('no configs files found in %s' % in_path)
+    except FileNotFoundError as err:
+        print('Error:', err)
