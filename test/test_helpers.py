4dbcdff20cf92c334791c41549d74c9f6a966fd2
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-01 16:42:11 +0530

    🧪 create_dir test

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,5 +1,6 @@
-import unittest
 import logging
+import unittest
+from unittest.mock import patch
 
 from clickgen import helpers
 
@@ -9,8 +10,14 @@ class TestHelpesMethods(unittest.TestCase):
         foo_logger = helpers.get_logger('foo')
         self.assertEqual(logging.getLogger('foo'), foo_logger)
 
-    def test_create_dir(parameter_list):
-        pass
+    @patch('clickgen.helpers.os.makedirs')
+    @patch('clickgen.helpers.os.path.exists')
+    def test_create_dir(self, mock_make_dirs, mock_exists):
+        mock_exists.return_value = True
+
+        helpers.create_dir('/foo/bar')
+
+        mock_make_dirs.assert_called_with('/foo/bar')
 
 
 if __name__ == '__main__':
