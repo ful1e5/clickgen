f260d6dfdd93686b2b2671fceec1d1942ed38cc3
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-02 10:41:10 +0530

    🧪 Context manager tested

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import unittest
 from unittest.mock import patch
 
@@ -6,6 +7,15 @@ from clickgen import helpers
 
 
 class TestHelpesMethods(unittest.TestCase):
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
+
+        self.orig_cwd = os.getcwd()
+        self.dst_dir = 'test'
+
+    def tearDown(self):
+        logging.disable(logging.NOTSET)
+
     def test_get_looger(self):
         foo_logger = helpers.get_logger('foo')
         self.assertEqual(logging.getLogger('foo'), foo_logger)
@@ -14,10 +24,20 @@ class TestHelpesMethods(unittest.TestCase):
     @patch('clickgen.helpers.os.path.exists')
     def test_create_dir(self, mock_make_dirs, mock_exists):
         mock_exists.return_value = True
-
         helpers.create_dir('/foo/bar')
-
         mock_make_dirs.assert_called_with('/foo/bar')
+
+    def test_TemporaryDirectory(self):
+        with helpers.TemporaryDirectory() as mock_tmp_dir:
+            self.assertTrue(os.path.isdir(mock_tmp_dir))
+
+    @patch('clickgen.helpers.os.chdir')
+    def test_cd(self, mock_chdir):
+        with helpers.cd(self.dst_dir):
+            mock_chdir.reset_mock()
+        mock_chdir.assert_called_once_with(self.orig_cwd)
+
+    # TODO:symblinks
 
 
 if __name__ == '__main__':
