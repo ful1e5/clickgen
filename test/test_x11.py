abdfe5bffb202ee1a4625cd5520dc096632b5b89
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-02 20:12:18 +0530

    🎊Iinit

---
+++ test/test_x11.py
@@ -0,0 +1,12 @@
+import unittest
+
+from clickgen import x11
+
+
+class TestX11Builder(unittest.TestCase):
+    def test_gen_argv_ctypes(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
