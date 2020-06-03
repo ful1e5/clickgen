eed2dc837363ef3f17d3b4cefad174b0f1ac4a32
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-03 11:08:16 +0530

    🧪 arguments cast test x11

--- test/test_x11.py
+++ test/test_x11.py
@@ -1,11 +1,19 @@
+import ctypes
 import unittest
+from unittest.mock import patch
 
 from clickgen import x11
+from clickgen.x11 import libpath
 
 
 class TestX11Builder(unittest.TestCase):
+    def setUp(self):
+        self.test_argv = ['foo', 'bar']
+
     def test_gen_argv_ctypes(self):
-        pass
+        mock_result = x11.gen_argv_ctypes(self.test_argv)
+        self.assertEqual(str(mock_result.__class__),
+                         "<class 'clickgen.x11.LP_LP_c_char'>")
 
 
 if __name__ == '__main__':
