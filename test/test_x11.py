5f4a3f37015f84beccbdeaf7d689d6908cdd0e84
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-03 11:11:58 +0530

    🧪 lib exists test

--- test/test_x11.py
+++ test/test_x11.py
@@ -1,4 +1,4 @@
-import ctypes
+import os
 import unittest
 from unittest.mock import patch
 
@@ -14,6 +14,9 @@ class TestX11Builder(unittest.TestCase):
         mock_result = x11.gen_argv_ctypes(self.test_argv)
         self.assertEqual(str(mock_result.__class__),
                          "<class 'clickgen.x11.LP_LP_c_char'>")
+
+    def test_generate(self):
+        self.assertTrue(os.path.exists(libpath))
 
 
 if __name__ == '__main__':
