0451930fa6c8c6d251d2bf4713c406a00b5ad114
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-04 17:31:47 +0530

    🧪 cursor.theme tested

--- test/test_template.py
+++ test/test_template.py
@@ -1,9 +1,12 @@
-
+import os
+import shutil
 import logging
+import tempfile
+
 import unittest
 
 import assets
-from clickgen import template
+from clickgen.template import __main__ as template
 
 
 class TestTemplate(unittest.TestCase):
@@ -12,12 +15,35 @@ class TestTemplate(unittest.TestCase):
     def setUp(self):
         logging.disable(logging.CRITICAL)
 
+        self.mock_dir = tempfile.mkdtemp()
+        self.mock_cursor_file_path = os.path.join(self.mock_dir,
+                                                  'cursor.theme')
+        self.mock_index_file_path = os.path.join(self.mock_dir, 'index.theme')
+        self.mock_name = 'foo'
+
     def tearDown(self):
         logging.disable(logging.NOTSET)
 
+        shutil.rmtree(self.mock_dir)
+
+    # helpers
+    def assert_word_in_file(self, file: str, word: str):
+        with open(file, 'r') as f:
+            content = f.read()
+            self.assertTrue(word in content)
+
     # tests
-    def test_main(self):
-        pass
+    def test_have_templates_files(self):
+        self.assertTrue(os.path.exists(template.cursor_file_path))
+        self.assertTrue(os.path.exists(template.index_file_path))
+
+    def test_create_x11_template_without_comments(self):
+        template.create_x11_template(name=self.mock_name,
+                                     dir=self.mock_dir,
+                                     comment='')
+        # testing cursor.theme
+        self.assert_word_in_file(file=self.mock_cursor_file_path,
+                                 word=self.mock_name)
 
 
 if __name__ == '__main__':
