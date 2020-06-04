369746ca590544ff5e6fcfcbf0f2dfc677e6285e
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-04 16:53:44 +0530

    🧪 Win builder test

--- test/test_win.py
+++ test/test_win.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import tempfile
 import unittest
 
 import assets
@@ -8,10 +11,14 @@ class TestWinBuilder(unittest.TestCase):
 
     # setup
     def setUp(self):
-        pass
+        self.temp_dir = tempfile.mkdtemp()
+        self.mock_prefix = assets.mock_config_path
+        self.mock_static_out = os.path.join(self.temp_dir, 'mock_static.cur')
+        self.mock_animated_out = os.path.join(self.temp_dir,
+                                              'mock_animated.ani')
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.temp_dir)
 
     # helpers
     def assert_cursor_size(self, path: str):
@@ -20,7 +27,17 @@ class TestWinBuilder(unittest.TestCase):
     # tests
     # TODO: More to test here
     def test_main(self):
-        pass
+        # testing static cursor
+        win.main(input_config=assets.get_static_mock_config_path(),
+                 output_file=self.mock_static_out,
+                 prefix=self.mock_prefix)
+        self.assert_cursor_size(self.mock_static_out)
+
+        # testing animated cursor
+        win.main(input_config=assets.get_animated_mock_config_path(),
+                 output_file=self.mock_animated_out,
+                 prefix=self.mock_prefix)
+        self.assert_cursor_size(self.mock_animated_out)
 
 
 if __name__ == '__main__':
