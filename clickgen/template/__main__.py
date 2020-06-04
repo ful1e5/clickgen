c06130ed11df2161f35557dffb7a7315521d10db
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-04 17:04:50 +0530

    💬 docs updated

--- clickgen/template/__main__.py
+++ clickgen/template/__main__.py
@@ -11,10 +11,9 @@ index_file_path = os.path.join(basedir, 'index.theme')
 
 
 def create_x11_template(dir: str, name: str, comment: str = '') -> None:
-    """Copy extra theme files to the directory.
-
+    """
+        Copy extra theme files to the directory.
         'dir' is a path to the cursor package directory.
-        
         'name' & 'comment' is Metadata for cursor package.
         Copy extra theme files to the directory.
         'dir' is a path to the cursor package directory.
