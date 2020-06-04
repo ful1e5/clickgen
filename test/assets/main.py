56874ce412e34f8cc8529802e369f3589d435032
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-04 08:51:08 +0530

    🗂 assets

--- test/assets/main.py
+++ test/assets/main.py
@@ -1,7 +1,12 @@
 import os
 
 basedir = os.path.dirname(__file__)
-mock_config_path=os.path.join(basedir,'configs')
+mock_config_path = os.path.join(basedir, 'configs')
+
+
+def get_static_mock_config_path() -> str:
+    return os.path.join(mock_config_path, 'mock_static.in')
+
 
 def get_animated_mock_config_path() -> str:
-    
+    return os.path.join(mock_config_path, 'mock_animated.in')
