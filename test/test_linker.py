3e10c16976b6c7af8ea9957e635d76425c34b76d
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-05 12:06:41 +0530

    🧪 Mock match to directory

--- test/test_linker.py
+++ test/test_linker.py
@@ -2,18 +2,29 @@ import logging
 
 import unittest
 
+from clickgen.linker import __main__ as linker
+
 
 class TestLinke(unittest.TestCase):
 
     # setup
     def setUp(self):
         logging.disable(logging.CRITICAL)
+        self.mock_directory = ['foo', 'bar', 'zoo']
 
     def tearDown(self):
         logging.disable(logging.NOTSET)
 
+    # helpers
+    def assert_to_directory(self, name: str, expect_name: str):
+        mock_result = linker.match_to_directory(name,
+                                                directory=self.mock_directory)
+        self.assertEqual(mock_result, expect_name)
+
+    # tests
     def test_match_to_directory(self):
-        pass
+        self.assert_to_directory(name='F0o', expect_name='foo')
+        # self.assert_to_mock_directory(name='F0o', expect_name='foo')
 
 
 if __name__ == "__main__":
