538834d66f4ef6ed967508815b3f43a69d25f410
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-09 17:47:42 +0530

    😎 direct access to configsgen in clickgen package

--- clickgen/__init__.py
+++ clickgen/__init__.py
@@ -5,8 +5,10 @@ import json
 import os
 
 from .__main__ import main as build, x11_gen, win_gen, link_cursors, create_x11_template
+
 from .linker import linker
 from .template import template
+from .configsgen import configsgen
 
 with open(os.path.join(os.path.dirname(__file__), 'pkginfo.json')) as fp:
     _info = json.load(fp)
