122f59eded8c149713b706303a431c66af421587
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-09 11:17:14 +0530

    🔧 Configsgen skeleton

--- clickgen/configsgen.py
+++ clickgen/configsgen.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def main():
-    pass
+from .types import Path, IntegerList
+
+
+def main(imgs_dir: Path, cursor_sizes: IntegerList, hotspots: any = None) -> Path:
+    """
+        Generate Configs files.
+        hotspots is JSON data for each cursor having xhot & yhot parameters.
+        Provide `None` value set hotspots to middle of cursor.
+        hotspots is default set to `None`
+    """
+
+    return'sdsd/sdsd.txt'
