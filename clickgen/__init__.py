a5e72285a32970fa4a9ee3fe37c66eaf043c4399
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-09 17:36:35 +0530

    😎 direct access to sub packages

--- clickgen/__init__.py
+++ clickgen/__init__.py
@@ -2,6 +2,8 @@ import json
 import os
 
 from .__main__ import main as build, x11_gen, win_gen, link_cursors, create_x11_template
+from .linker import linker
+from .template import template
 
 with open(os.path.join(os.path.dirname(__file__), 'pkginfo.json')) as fp:
     _info = json.load(fp)
