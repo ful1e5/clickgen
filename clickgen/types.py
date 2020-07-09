19d463f8c67765087b56b3a1591fa2f1fa441fcb
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-09 17:22:04 +0530

    🏷 Sizes typing

--- clickgen/types.py
+++ clickgen/types.py
@@ -2,10 +2,12 @@
 # encoding: utf-8
 
 import pathlib
-from typing import List, Union
+from typing import List, Union, Tuple
 
 Path = Union[str, pathlib.Path]
 
 StringList = List[str]
 
 IntegerList = List[int]
+
+IntegerTuple = Tuple[int, int]
