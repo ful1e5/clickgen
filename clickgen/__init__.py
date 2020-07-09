41843d1088360ebf0160ca5ceeadaedcca1c5792
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-09 17:26:48 +0530

    😎 More fuctions access from clickgen

--- clickgen/__init__.py
+++ clickgen/__init__.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from .__main__ import main
+from .__main__ import main as build, x11_gen, win_gen, link_cursors, create_x11_template
 
 with open(os.path.join(os.path.dirname(__file__), 'pkginfo.json')) as fp:
     _info = json.load(fp)
