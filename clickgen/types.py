ed60eca9f79c6af7b110018ea4ea8f338ea8ac0b
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-11 09:21:35 +0530

    📜 Logger type

--- clickgen/types.py
+++ clickgen/types.py
@@ -2,11 +2,13 @@
 # encoding: utf-8
 
 import pathlib
+from logging import Logger
 from typing import List, Union, Tuple
 
 Path = Union[str, pathlib.Path]
 
 StringList = List[str]
+
 
 IntegerList = List[int]
 
