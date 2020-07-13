7b1a466f01f6c6b6beec27e6ec85887008151e8d
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-13 11:47:57 +0530

    📦 Archive flag in build

--- clickgen/build.py
+++ clickgen/build.py
@@ -9,31 +9,31 @@ from .types import Path, IntegerList
 
 
 # TODO: Tests
-def build_win_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+def build_win_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, out_path: Path = os.getcwd(), archive: bool = False):
 
     # generate configs
     configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
 
     main(name=name, config_dir=configs, out_path=out_path,
-         x11=False, win=True, logs=logs)
+         x11=False, win=True, archive=archive, logs=True)
 
 
 # TODO: Tests
-def build_x11_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+def build_x11_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, out_path: Path = os.getcwd(), archive: bool = False):
 
     # generate configs
     configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
 
     main(name=name, config_dir=configs, out_path=out_path,
-         x11=True, win=False, logs=logs)
+         x11=True, win=False, archive=archive, logs=True)
 
 
 # TODO: Tests
-def build_cursor_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+def build_cursor_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, out_path: Path = os.getcwd()):
 
     # generate configs
     configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
 
     # build package
     main(name=name, config_dir=configs, out_path=out_path,
-         x11=True, win=True, archive=archive, logs=logs)
+         x11=True, win=True, archive=True, logs=True)
