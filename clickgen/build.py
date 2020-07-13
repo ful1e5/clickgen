8707011d242a41c29c1f59bd6eedfc9eaeb34697
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-13 10:25:52 +0530

    📦 build module

---
+++ clickgen/build.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+
+from .__main__ import main
+from .configsgen.__main__ import main as configsgen
+from .types import Path, IntegerList
+
+
+# TODO: Tests
+def build_win_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+
+    # generate configs
+    configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
+
+    main(name=name, config_dir=configs, out_path=out_path,
+         x11=False, win=True, logs=logs)
+
+
+# TODO: Tests
+def build_x11_curosr_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+
+    # generate configs
+    configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
+
+    main(name=name, config_dir=configs, out_path=out_path,
+         x11=True, win=False, logs=logs)
+
+
+# TODO: Tests
+def build_cursor_theme(name: str, image_dir: Path, cursor_sizes: IntegerList, hotspots: any = None, archive: bool = False, out_path: Path = os.getcwd(), logs: bool = False):
+
+    # generate configs
+    configs: Path = configsgen(image_dir, cursor_sizes, hotspots)
+
+    # build package
+    main(name=name, config_dir=configs, out_path=out_path,
+         x11=True, win=True, archive=archive, logs=logs)
