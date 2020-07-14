3eb80f202534fff89d8e619d540efeede6b33ee7
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-14 10:11:52 +0530

    🧪 Test static list

--- test/test_configsgen.py
+++ test/test_configsgen.py
@@ -2,8 +2,16 @@
 # encoding: utf-8
 import unittest
 
+from . import assets
+from clickgen import configsgen
+
 
 class TestConfigsgen(unittest.TestCase):
+
     # tests
     def test_get_cursor_list(self):
-        pass
+        # testing static images list
+        mock_static_list = configsgen.get_cursor_list(
+            imgs_dir=assets.get_mock_images_path(), animated=False)
+
+        self.assertEqual(mock_static_list, ['mock_static.png'])
