9c980f4b6baad4341f13b4b740c6019a99bf1ae8
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-15 16:35:12 +0530

    🖼 images lists static and animated

--- test/assets/__main__.py
+++ test/assets/__main__.py
@@ -25,8 +25,26 @@ def get_mock_image() -> str:
     return os.path.join(basedir, 'images', 'mock_static.png')
 
 
-def get_mock_images_list() -> [str]:
-    return os.listdir(os.path.join(basedir, 'images'))
+def get_mock_animated_images_list() -> [str]:
+    images: [str] = os.listdir(os.path.join(basedir, 'images'))
+    animated_list: [str] = []
+
+    for image in images:
+        if '-' in image:
+            animated_list.append(image)
+
+    return animated_list
+
+
+def get_mock_static_images_list() -> [str]:
+    images: [str] = os.listdir(os.path.join(basedir, 'images'))
+    animated_list: [str] = []
+
+    for image in images:
+        if '-' not in image:
+            animated_list.append(image)
+
+    return animated_list
 
 
 def get_mock_hotspots() -> any:
