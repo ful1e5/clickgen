6e031da079ddfa9a6968dae9b3633d8401818a51
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-21 12:16:33 +0530

    🧹 File cleanup

--- pkginfo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-package_name = 'clickgen'
-package_version = '1.1.2'
-package_description = 'X11 & Windows Cursor API 👷'
-package_author = 'Kaiz Khatri'
-package_author_email = 'kaizmandhu@gmail.com'
-package_status_classifier = 'Development Status :: 3 - Alpha'
