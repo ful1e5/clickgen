ffc80139aa7dbbd47d4c06f860bd3db5bdb6eda9
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-15 12:41:10 +0530

    🖼️ png files list method

--- clickgen/providers/cursors_config.py
+++ clickgen/providers/cursors_config.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from glob import glob
+from os import PathLike, path
+from typing import AnyStr, List, Union, Callable
 from clickgen.providers.json_parser import HotspotsParser
-from os import PathLike
-from typing import AnyStr, Union
 
 
 class CursorsConfigFileProvider:
     """ Configure `clickgen` cursor building process. """
 
-    def __init__(
-        self, bitmaps_dir: PathLike[AnyStr], hotspots_file=PathLike[AnyStr]
-    ) -> None:
+    def __init__(self, bitmaps_dir: str, hotspots_file=PathLike[AnyStr]) -> None:
         self.__bitmaps_dir = bitmaps_dir
-        self.__cor_parser = HotspotsParser(hotspots_file)
+        self.__cords_parser = HotspotsParser(hotspots_file)
 
-    def __list_static_cursors(self, is_animated: bool = False) -> list[str]:
-        """ Return cursors list inside `bitmaps_dir`"""
-        l: list[str] = ["a", "b"]
-        return l
+    def __get_png_files(self) -> List[str]:
+        """ Return list of .png files in `bitmaps_dir`. """
+        func: Callable[[str], str] = lambda x: path.basename(x)
+        pngs: List[str] = []
+
+        try:
+            pngs.extend(list(map(func, glob(path.join(self.__bitmaps_dir, "*.png")))))
+            if len(pngs) <= 0:
+                raise Exception("Cursors .png files not found")
+        except Exception as e:
+            print(e)
+
+        return pngs
+
+    def __list_static_cursors(self, is_animated: bool = False) -> List[str]:
+        """ Return cursors list inside `bitmaps_dir` that doesn't had frames. """
+        pngs: List[str] = self.__get_png_files()
+
+        return pngs
 
     def get_config(self, cursor_name: str) -> Union[PathLike[AnyStr], str]:
-        """ Return Config(`.in`) file path. """
+        """ Return `.in` file path. """
         return cursor_name
