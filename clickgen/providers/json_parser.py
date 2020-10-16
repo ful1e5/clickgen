96ab955bfc91e33397aae4e935633a3ace6a0000
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-16 17:12:16 +0530

    🏷 Typing Fix

--- clickgen/providers/json_parser.py
+++ clickgen/providers/json_parser.py
@@ -2,16 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import json
-from os import PathLike
-from typing import AnyStr, Tuple, Union
+from typing import AnyStr, Tuple
 
 
 class HotspotsParser:
     """ Parse json file,that contains the hotspots."""
 
-    def __init__(self, f: PathLike[AnyStr]) -> None:
-        with open(f, "r") as data:
-            self.__hotspots = json.loads(data.read())
+    def __init__(self, f: AnyStr) -> None:
+        data = open(f, "r")
+        self.__hotspots = json.loads(data.read())
 
     def get_hotspots(self, c: str, old_size: int, new_size: int) -> Tuple[int, int]:
         xhot = yhot = int(new_size / 2)
