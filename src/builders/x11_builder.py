58ffb017dc90fba47d6a99ccb6cba24c0da32b75
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-19 12:24:51 +0530

    👷 init X11 cursor builder

---
+++ src/builders/x11_builder.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ctypes
+from ctypes import CDLL, _CData, pointer
+from os import path
+from typing import List, Type
+
+lib_path: str = path.join(__file__, path.abspath("../../libs/xcursorgen.so"))
+xcursorgen: CDLL = CDLL(lib_path)
+
+# main function ctypes define
+LP_c_char: Type[pointer[_CData]] = ctypes.POINTER(ctypes.c_char)
+LP_LP_c_char: Type[pointer[_CData]] = ctypes.POINTER(LP_c_char)
+xcursorgen.main.argtypes = (ctypes.c_int, LP_LP_c_char)
+
+
+def gen_argv_ctypes(argv: List[str]) -> pointer[_CData]:
+    """ Convert `string` arguments to `ctypes` pointer. """
+    p = (LP_c_char * len(argv))()
+
+    for i, arg in enumerate(argv):
+        enc_arg: bytes = str(arg).encode("utf-8")
+        p[i] = ctypes.create_string_buffer(enc_arg)
+
+    return ctypes.cast(p, LP_LP_c_char)
+
+
+def generate_x11_cursor(cfg_file_path: str, out_path: str, prefix: str) -> None:
+    """ Generate x11 cursor from `.in` file."""
+    argv: List[str] = ["xcursorgen", "-p", prefix, cfg_file_path, out_path]
+
+    kwargs: pointer[_CData] = gen_argv_ctypes(argv)
+    args: ctypes.c_int = ctypes.c_int(len(argv))
+
+    xcursorgen.main(args, kwargs)
