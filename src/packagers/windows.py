00c6537a4c491d7f0b9e0fb15b086cd4c0520194
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-28 09:24:51 +0530

    📦 Windows packager init

---
+++ src/packagers/windows.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class WindowsPackager:
+    """ Create a crispy `Windows` cursor theme package. """
+
+    def __init__(self) -> None:
+        pass
