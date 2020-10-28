0dbb71c2e29999d09b050427c3868a4a50420711
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-28 18:26:21 +0530

    🚀 theme creator method init

--- src/clickgen.py
+++ src/clickgen.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from .configs.core import Config
+
+
+def create_theme(config: Config) -> None:
+    """ Create Cursors theme from `bitmaps`.  """
+    pass
