9d26568b1b9f9f9e7480e411a79a408705d9e1c7
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-29 11:35:49 +0530

    ⚡ Cursor theme builder helper

--- clickgen/clickgen.py
+++ clickgen/clickgen.py
@@ -1,9 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .configs import Config
+from os import makedirs, path
+import shutil
+import tempfile
+
+from .builders.winbuilder import WinCursorsBuilder
+from .builders.x11builder import X11CursorsBuilder
+from .configs import BuildSettings, Config, ThemeInfo
+from .packagers.windows import WindowsPackager
+from .packagers.x11 import X11Packager
+from .providers.themeconfig import ThemeConfigsProvider
 
 
 def create_theme(config: Config) -> None:
-    """ Create Cursors theme from `bitmaps`.  """
-    pass
+    """ Create cursors theme from `bitmaps`.  """
+    info: ThemeInfo = config.info
+    sett: BuildSettings = config.settings
+
+    # Cursors '.in' files generator
+    config_dir: str = ThemeConfigsProvider(
+        bitmaps_dir=sett.bitmaps_dir,
+        hotspots=sett.hotspots,
+        sizes=sett.sizes,
+    ).generate(sett.animation_delay)
+
+    # Setup temporary directories
+    xtmp: str = tempfile.mkdtemp(prefix="xbu")
+    wtmp: str = tempfile.mkdtemp(prefix="wbu")
+
+    # Building Themes
+    WinCursorsBuilder(config_dir, wtmp).build()
+    WindowsPackager(wtmp, info).pack()
+
+    X11CursorsBuilder(config_dir, xtmp).build()
+    X11Packager(xtmp, info).pack()
+
+    # Move themes to @out_dir
+    if not path.exists(sett.out_dir):
+        makedirs(sett.out_dir)
+
+    xdst: str = path.join(sett.out_dir, info.theme_name)
+    if path.exists(xdst):
+        shutil.rmtree(xdst)
+    shutil.move(xtmp, xdst)
+
+    wdst: str = path.join(sett.out_dir, f"{info.theme_name}-Windows")
+    if path.exists(wdst):
+        shutil.rmtree(wdst)
+    shutil.move(wtmp, wdst)
