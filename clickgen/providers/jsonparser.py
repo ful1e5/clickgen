60699440f62832347076683f7b97fa82ff9e6512
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-31 10:33:25 +0530

    🚨 Key value exception handling

--- clickgen/providers/jsonparser.py
+++ clickgen/providers/jsonparser.py
@@ -18,15 +18,20 @@ class HotspotsParser:
     def get_hotspots(
         self, c: str, old_size: Tuple[int, int], new_size: int
     ) -> Tuple[int, int]:
+        try:
+            x = self.__hotspots[c]["xhot"]
+            y = self.__hotspots[c]["yhot"]
+            (width, height) = old_size
 
-        x = self.__hotspots[c]["xhot"]
-        y = self.__hotspots[c]["yhot"]
-        (width, height) = old_size
-
-        if x is None or y is None:
-            xhot = yhot = int(new_size / 2)
-        else:
             xhot = int(round(new_size / width * x))
             yhot = int(round(new_size / height * y))
 
-        return (xhot, yhot)
+            return (xhot, yhot)
+
+        except KeyError as key:
+            xhot = yhot = int(new_size / 2)
+            print(
+                f"{key} hotspots not provided for {new_size}x{new_size}, Setting to ({xhot},{yhot})"
+            )
+
+            return (xhot, yhot)
