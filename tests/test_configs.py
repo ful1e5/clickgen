3e6e01ee158959585a529546de72d53f3933a11f
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-01 17:04:01 +0530

    🧪 test for ThemeSettings

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from clickgen.configs import ThemeInfo
+import os
+from typing import Dict
+
+from clickgen.configs import ThemeInfo, ThemeSettings
 
 
 def test_theme_info():
@@ -12,3 +15,21 @@ def test_theme_info():
     assert isinstance(t.theme_name, str)
     assert isinstance(t.author, str)
     assert t.url == "Unknown Source!"
+
+
+def test_theme_settings():
+    ts = ThemeSettings(
+        bitmaps_dir="foo", sizes=[1, 2], hotspots={"a": {"xhot": 1, "yhot": 2}}
+    )
+
+    assert isinstance(ts, ThemeSettings)
+
+    assert isinstance(ts.sizes[0], int)
+
+    assert ts.animation_delay == 50
+    assert ts.out_dir == os.getcwd()
+
+    assert isinstance(ts.hotspots, Dict)
+    assert isinstance(ts.hotspots.get("a"), Dict)
+    assert isinstance(ts.hotspots.get("a").get("xhot"), int)
+    assert isinstance(ts.hotspots.get("a").get("yhot"), int)
