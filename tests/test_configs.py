5bdbd2db40fe241432ba773d8faaee2ed7076c61
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-01 17:26:24 +0530

    🧪 TestCase based test

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -3,33 +3,35 @@
 
 import os
 from typing import Dict
+import unittest
 
 from clickgen.configs import ThemeInfo, ThemeSettings
 
 
-def test_theme_info():
-    t = ThemeInfo(theme_name="foo", author="bar")
-    assert isinstance(t, ThemeInfo)
+class ConfigTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.t = ThemeInfo(theme_name="foo", author="bar")
+        self.ts = ThemeSettings(
+            bitmaps_dir="foo", sizes=[1, 2], hotspots={"a": {"xhot": 1, "yhot": 2}}
+        )
 
-    assert t.comment == None
-    assert isinstance(t.theme_name, str)
-    assert isinstance(t.author, str)
-    assert t.url == "Unknown Source!"
+    def test_theme_info(self) -> None:
+        assert isinstance(self.t, ThemeInfo)
 
+        assert self.t.comment == None
+        assert isinstance(self.t.theme_name, str)
+        assert isinstance(self.t.author, str)
+        assert self.t.url == "Unknown Source!"
 
-def test_theme_settings():
-    ts = ThemeSettings(
-        bitmaps_dir="foo", sizes=[1, 2], hotspots={"a": {"xhot": 1, "yhot": 2}}
-    )
+    def test_theme_settings(self) -> None:
+        assert isinstance(self.ts, ThemeSettings)
 
-    assert isinstance(ts, ThemeSettings)
+        assert isinstance(self.ts.sizes[0], int)
 
-    assert isinstance(ts.sizes[0], int)
+        assert self.ts.animation_delay == 50
+        assert self.ts.out_dir == os.getcwd()
 
-    assert ts.animation_delay == 50
-    assert ts.out_dir == os.getcwd()
-
-    assert isinstance(ts.hotspots, Dict)
-    assert isinstance(ts.hotspots.get("a"), Dict)
-    assert isinstance(ts.hotspots.get("a").get("xhot"), int)
-    assert isinstance(ts.hotspots.get("a").get("yhot"), int)
+        assert isinstance(self.ts.hotspots, Dict)
+        assert isinstance(self.ts.hotspots.get("a"), Dict)
+        assert isinstance(self.ts.hotspots.get("a").get("xhot"), int)
+        assert isinstance(self.ts.hotspots.get("a").get("yhot"), int)
