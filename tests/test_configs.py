5b3e605938ad7cc5e1a9bf05c765e7c699e889a5
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-02 09:53:20 +0530

    🧪 using @pytest.fixture

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -3,35 +3,41 @@
 
 import os
 from typing import Dict
-import unittest
 
 from clickgen.configs import ThemeInfo, ThemeSettings
+import pytest
 
 
-class ConfigTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.t = ThemeInfo(theme_name="foo", author="bar")
-        self.ts = ThemeSettings(
-            bitmaps_dir="foo", sizes=[1, 2], hotspots={"a": {"xhot": 1, "yhot": 2}}
-        )
+@pytest.fixture
+def ti() -> ThemeInfo:
+    return ThemeInfo(theme_name="foo", author="bar")
 
-    def test_theme_info(self) -> None:
-        assert isinstance(self.t, ThemeInfo)
 
-        assert self.t.comment == None
-        assert isinstance(self.t.theme_name, str)
-        assert isinstance(self.t.author, str)
-        assert self.t.url == "Unknown Source!"
+@pytest.fixture
+def ts() -> ThemeSettings:
+    return ThemeSettings(
+        bitmaps_dir="foo", sizes=[1, 2], hotspots={"a": {"xhot": 1, "yhot": 2}}
+    )
 
-    def test_theme_settings(self) -> None:
-        assert isinstance(self.ts, ThemeSettings)
 
-        assert isinstance(self.ts.sizes[0], int)
+def test_theme_info(ti) -> None:
+    assert isinstance(ti, ThemeInfo)
 
-        assert self.ts.animation_delay == 50
-        assert self.ts.out_dir == os.getcwd()
+    assert ti.comment == None
+    assert isinstance(ti.theme_name, str)
+    assert isinstance(ti.author, str)
+    assert ti.url == "Unknown Source!"
 
-        assert isinstance(self.ts.hotspots, Dict)
-        assert isinstance(self.ts.hotspots.get("a"), Dict)
-        assert isinstance(self.ts.hotspots.get("a").get("xhot"), int)
-        assert isinstance(self.ts.hotspots.get("a").get("yhot"), int)
+
+def test_theme_settings(ts) -> None:
+    assert isinstance(ts, ThemeSettings)
+
+    assert isinstance(ts.sizes[0], int)
+
+    assert ts.animation_delay == 50
+    assert ts.out_dir == os.getcwd()
+
+    assert isinstance(ts.hotspots, Dict)
+    assert isinstance(ts.hotspots.get("a"), Dict)
+    assert isinstance(ts.hotspots.get("a").get("xhot"), int)
+    assert isinstance(ts.hotspots.get("a").get("yhot"), int)
