be9c3f151c58ae176d1e8b65547010b7ee59b1c1
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-03 11:15:05 +0530

    🧪 More test cases added

--- tests/test__themeconfig.py
+++ tests/test__themeconfig.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
 from typing import List, Tuple
+
+import pytest
+
 from clickgen.providers.themeconfig import _clean_cur_name
 
 testdata: List[Tuple[str, str]] = [
     ("/foo/bar-01", "/foo/bar"),
     ("/foo/b-000000001", "/foo/b"),
     ("/foo/b", "/foo/b"),
+    ("/foo/bsd-gg", "/foo/bsd"),
+    ("/foo/rr.ds=ds-rf", "/foo/brr.ds=ds"),
 ]
 
 
