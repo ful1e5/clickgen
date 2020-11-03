4cfa7868b2ee607aece116697ecda7d9c086ffa1
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-03 17:14:42 +0530

    🧪 Test "cords" in ThemeConfigProvider

--- tests/test__themeconfig.py
+++ tests/test__themeconfig.py
@@ -6,6 +6,7 @@ from unittest.mock import PropertyMock, patch
 
 import pytest
 
+from clickgen.providers.jsonparser import HotspotsParser
 from clickgen.providers.themeconfig import ThemeConfigsProvider, _clean_cur_name
 
 clean_cur_name_parameters: List[Tuple[str, str]] = [
@@ -22,7 +23,7 @@ def test_clean_cur_name(arg, result) -> None:
     assert _clean_cur_name(arg) == result
 
 
-def test_themeconfig_provider_sizes(bitmaps_dir, sizes, hotspots) -> None:
+def test_theme_configs_provider_sizes(bitmaps_dir, sizes, hotspots) -> None:
     with patch.object(
         ThemeConfigsProvider, "_ThemeConfigsProvider__sizes", new_callable=PropertyMock
     ) as mock_sizes:
@@ -32,7 +33,7 @@ def test_themeconfig_provider_sizes(bitmaps_dir, sizes, hotspots) -> None:
         assert sizes == tcp._ThemeConfigsProvider__sizes  # type: ignore
 
 
-def test_themeconfig_provider_bitmaps_dir(bitmaps_dir, sizes, hotspots) -> None:
+def test_theme_configs_provider_bitmaps_dir(bitmaps_dir, sizes, hotspots) -> None:
     with patch.object(
         ThemeConfigsProvider,
         "_ThemeConfigsProvider__bitmaps_dir",
@@ -42,3 +43,15 @@ def test_themeconfig_provider_bitmaps_dir(bitmaps_dir, sizes, hotspots) -> None:
 
         mock_bitmaps_dir.return_value = bitmaps_dir
         assert bitmaps_dir == tcp._ThemeConfigsProvider__bitmaps_dir  # type: ignore
+
+
+def test_theme_configs_provider_cords(bitmaps_dir, sizes, hotspots) -> None:
+    with patch.object(
+        ThemeConfigsProvider,
+        "_ThemeConfigsProvider__cords",
+        new_callable=PropertyMock,
+    ) as mock_hotspots:
+        tcp = ThemeConfigsProvider(bitmaps_dir, mock_hotspots, sizes)
+
+        mock_hotspots.return_value = HotspotsParser(hotspots)
+        assert isinstance(tcp._ThemeConfigsProvider__cords, HotspotsParser)  # type: ignore
