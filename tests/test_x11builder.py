50fee66db342eae9a13b38b41c699b9efd89b9dc
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-05 10:36:37 +0530

    🧪 Test xcursors

--- tests/test_x11builder.py
+++ tests/test_x11builder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+from os import path
 from unittest.mock import PropertyMock, patch
 
 from clickgen.builders.x11builder import X11CursorsBuilder
@@ -22,3 +24,19 @@ def test_x11builder_out_dir(mock_out_dir) -> None:
     x = X11CursorsBuilder("/foo/", mock_out_dir)
     mock_out_dir.return_value = "/foo/out/"
     assert "/foo/out/" == x._X11CursorsBuilder__out_dir  # type: ignore
+
+
+def test_x11builder_out_dir_files(config_dir, out_dir) -> None:
+    X11CursorsBuilder(config_dir, out_dir).build()
+    assert len(os.listdir(out_dir)) > 0
+
+
+def test_x11builder_out_dir_contain_cursors_directory(xcursors_dir) -> None:
+    assert path.exists(xcursors_dir)
+
+
+def test_x11builder_out_dir_xcursors(xcursors_dir, hotspots) -> None:
+    curs = os.listdir(xcursors_dir)
+
+    for c in curs:
+        assert c in hotspots.keys()
