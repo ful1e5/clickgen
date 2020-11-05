85e0b26761bc8f4f1c0691aca6aa6f7273cb6e69
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-05 10:12:06 +0530

    🧪 Test __config_dir member access

--- tests/test_x11builder.py
+++ tests/test_x11builder.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from tests.conftest import config_dir, out_dir
 from clickgen.builders.x11builder import X11CursorsBuilder
+from unittest.mock import PropertyMock, patch
 
 
-def test_x11builder(config_dir, out_dir) -> None:
-    X11CursorsBuilder(config_dir, out_dir)
+@patch.object(
+    X11CursorsBuilder, "_X11CursorsBuilder__config_dir", new_callable=PropertyMock
+)
+def test_x11builder_config_dir(mock_config_dir) -> None:
+    x = X11CursorsBuilder(mock_config_dir, "/foo/")
+    mock_config_dir.return_value = "/foo/out/"
+    assert "/foo/out/" == x._X11CursorsBuilder__config_dir  # type: ignore
