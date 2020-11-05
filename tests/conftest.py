91c95539aaf8074d2df1f25aa0900707f9bb7a0e
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-05 10:36:22 +0530

    📦 xcursors_dir fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from os import path
 from typing import List
 
 import pytest
+from clickgen.builders.x11builder import X11CursorsBuilder
 from clickgen.configs import ThemeInfo
 from clickgen.providers.jsonparser import Hotspots
 from clickgen.providers.themeconfig import ThemeConfigsProvider
@@ -36,6 +37,12 @@ def bitmaps_dir() -> str:
 @pytest.fixture(scope="module")
 def out_dir() -> str:
     return tempfile.mkdtemp()
+
+
+@pytest.fixture(scope="module")
+def xcursors_dir(config_dir, out_dir) -> str:
+    X11CursorsBuilder(config_dir, out_dir).build()
+    return path.join(out_dir, "cursors")
 
 
 @pytest.fixture(scope="module")
