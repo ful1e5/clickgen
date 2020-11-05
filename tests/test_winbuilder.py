6a9287ce845f9a4c546a37f2f945507067367ab3
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-05 17:07:30 +0530

    🧪 Test __out_dir access

--- tests/test_winbuilder.py
+++ tests/test_winbuilder.py
@@ -12,3 +12,12 @@ def test_winbuilder_config_dir(mock_config_dir) -> None:
     x = WinCursorsBuilder(mock_config_dir, "/foo/out")
     mock_config_dir.return_value = "/foo/"
     assert "/foo/" == x._WinCursorsBuilder__config_dir  # type: ignore
+
+
+@patch.object(
+    WinCursorsBuilder, "_WinCursorsBuilder__out_dir", new_callable=PropertyMock
+)
+def test_x11builder_out_dir(mock_out_dir) -> None:
+    x = WinCursorsBuilder("/foo/", mock_out_dir)
+    mock_out_dir.return_value = "/foo/out/"
+    assert "/foo/out/" == x._WinCursorsBuilder__out_dir  # type: ignore
