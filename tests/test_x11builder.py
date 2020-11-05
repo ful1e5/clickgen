e42d24d99783479530aaef1e520917537c61252d
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-05 10:22:39 +0530

    🧪 Test private member in X11Builder

--- tests/test_x11builder.py
+++ tests/test_x11builder.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tests.conftest import config_dir, out_dir
-from clickgen.builders.x11builder import X11CursorsBuilder
 from unittest.mock import PropertyMock, patch
+
+from clickgen.builders.x11builder import X11CursorsBuilder
 
 
 @patch.object(
     X11CursorsBuilder, "_X11CursorsBuilder__config_dir", new_callable=PropertyMock
 )
 def test_x11builder_config_dir(mock_config_dir) -> None:
-    x = X11CursorsBuilder(mock_config_dir, "/foo/")
-    mock_config_dir.return_value = "/foo/out/"
-    assert "/foo/out/" == x._X11CursorsBuilder__config_dir  # type: ignore
+    x = X11CursorsBuilder(mock_config_dir, "/foo/out")
+    mock_config_dir.return_value = "/foo/"
+    assert "/foo/" == x._X11CursorsBuilder__config_dir  # type: ignore
+
+
+@patch.object(
+    X11CursorsBuilder, "_X11CursorsBuilder__out_dir", new_callable=PropertyMock
+)
+def test_x11builder_out_dir(mock_out_dir) -> None:
+    x = X11CursorsBuilder("/foo/", mock_out_dir)
+    mock_out_dir.return_value = "/foo/out/"
+    assert "/foo/out/" == x._X11CursorsBuilder__out_dir  # type: ignore
