af0f22f3a8a925cfa9a8134780c7b1f8d345930c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-08 09:47:03 +0530

    🧪 X11 Package Full test

--- tests/test_x11_packager.py
+++ tests/test_x11_packager.py
@@ -8,8 +8,18 @@ import pytest
 from clickgen.packagers.x11 import X11Packager
 
 
-def test_x11_package(xcursors_dir, ti) -> None:
-    dir = path.abspath(path.join(xcursors_dir, "../"))
-    X11Packager(dir, ti).pack()
+def test_x11_package(xcursors_dir, ti, hotspots) -> None:
+    directory = path.abspath(path.join(xcursors_dir, "../"))
+    X11Packager(directory, ti).pack()
 
-    assert pytest.approx(os.listdir(dir) == ["cursors", "index.theme", "cursor.theme"])
+    assert pytest.approx(
+        os.listdir(directory) == ["cursors", "index.theme", "cursor.theme"]
+    )
+
+    assert pytest.approx(os.listdir(xcursors_dir) == hotspots.keys())
+
+    i_lns = open(path.join(directory, "index.theme"), "r").readlines()
+    assert i_lns == ["[Icon Theme]\n", "Name=foo\n", "Comment=None"]
+
+    c_lns = open(path.join(directory, "cursor.theme"), "r").readlines()
+    assert c_lns == ["[Icon Theme]\n", "Inherits=foo"]
