ab0d88d8fac20c39ebe0009bcccb7c17100ffd31
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-11 12:21:08 +0530

    🖼️ Bitmaps provider

---
+++ clickgen/providers/bitmaps.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from glob import glob
+from os import path
+from typing import Callable, Dict, List
+
+
+class ThemeBitmapsProvider:
+    """ Provide cursors bitmaps."""
+
+    dir: str = ""
+
+    def __init__(self, bitmaps_dir) -> None:
+        self.dir = bitmaps_dir
+
+    def get_png_files(self) -> List[str]:
+        """ Return list of .png files in `bitmaps_dir`. """
+        func: Callable[[str], str] = lambda x: path.basename(x)
+        pngs: List[str] = []
+
+        try:
+            pngs.extend(list(map(func, glob(path.join(self.dir, "*.png")))))
+            if len(pngs) <= 0:
+                raise FileNotFoundError("Cursors .png files not found")
+        except FileNotFoundError as e:
+            print(e)
+
+        return pngs
+
+    def static_bitmaps(self) -> List[str]:
+        """ Return cursors list inside `bitmaps_dir` that doesn't had frames. """
+        func: Callable[[str], bool] = lambda x: x.find("-") <= 0
+        st_pngs: List[str] = list(filter(func, self.get_png_files()))
+
+        return st_pngs
+
+    def animated_bitmaps(self) -> Dict[str, List[str]]:
+        """ Return cursors list inside `bitmaps_dir` that had frames. """
+        func: Callable[[str], bool] = lambda x: x.find("-") >= 0
+        an_pngs: List[str] = list(filter(func, self.get_png_files()))
+
+        g_func: Callable[[str], str] = lambda x: x.split("-")[0]
+        grps: List[str] = list(set(map(g_func, an_pngs)))
+
+        d: Dict[str, List[str]] = {}
+
+        for g in grps:
+            func: Callable[[str], bool] = lambda x: x.find(g) >= 0
+            d[g] = sorted(list(filter(func, an_pngs)))
+
+        return d
