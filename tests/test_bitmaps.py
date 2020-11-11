75aac2b8bb764f3c83816a5545a033ac057954b8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-11 12:33:08 +0530

    🧪 test ThemeBitmapsProvider

--- tests/test_bitmaps.py
+++ tests/test_bitmaps.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from clickgen.providers.bitmaps import ThemeBitmapsProvider
 
 
 def test_theme_bitmaps_provider_dir(bitmaps_dir: str) -> None:
     t = ThemeBitmapsProvider(bitmaps_dir)
     assert t.dir == bitmaps_dir
+
+
+def test_theme_bitmaps_provider_animated_bitmaps(
+    bitmaps_provider: ThemeBitmapsProvider,
+) -> None:
+    assert bitmaps_provider.animated_bitmaps() == {"c": ["c-01.png", "c-02.png"]}
+
+
+def test_theme_bitmaps_provider_static_bitmaps(
+    bitmaps_provider: ThemeBitmapsProvider,
+) -> None:
+    assert bitmaps_provider.static_bitmaps() == ["a.png", "b.png"]
