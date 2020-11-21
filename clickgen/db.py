fcb5c295ab437a26b2517f7445fc3b267fa39e37
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-21 12:30:57 +0530

    🗃 DB Dublication avoid

--- clickgen/db.py
+++ clickgen/db.py
@@ -2,56 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os import path
 import tempfile
 from difflib import SequenceMatcher as SM
 from typing import List, NamedTuple, Optional
 
 from tinydb import TinyDB
-from tinydb.queries import Query, where
+from tinydb.queries import where
 from tinydb.table import Document
 
 seed_data = [
-    {
-        "name": "default",
-        "symlink": ["default", "left_ptr", "top_left_arrow", "left-arrow"],
-    },
-    {
-        "name": "arrow",
-        "symlink": ["arrow", "right_ptr", "top_right_arrow", "right-arrow"],
-    },
-    {"name": "center_ptr", "symlink": ["center_ptr"]},
-    {
-        "name": "link",
-        "symlink": [
-            "link",
-            "alias",
-            "0876e1c15ff2fc01f906f1c363074c0f",
-            "3085a0e285430894940527032f8b26df",
-            "640fb0e74195791501fd1ed57b41487f",
-            "a2a266d0498c3104214a47bd64ab0fc8",
-        ],
-    },
-    {"name": "dnd_link", "symlink": ["dnd-link"]},
-    {"name": "forbidden", "symlink": ["forbidden", "not-allowed"]},
-    {
-        "name": "crossed_circle",
-        "symlink": ["crossed_circle", "03b6e0fcb3499374a867c041f52298f0"],
-    },
-    {"name": "circle", "symlink": ["circle"]},
-    {
-        "name": "dnd_no_drop",
-        "symlink": [
-            "dnd-no-drop",
-            "no-drop",
-            "03b6e0fcb3499374a867c041f52298f0",
-            "03b6e0fcb3499374a867d041f52298f0",
-        ],
-    },
-    {"name": "pirate", "symlink": ["pirate", "kill"]},
-    {"name": "pencil", "symlink": ["pencil"]},
+    # Animated cursors
     {
         "name": "wait",
-        "symlink": ["wait", "watch", "clock", "0426c94ea35c87780ff01dc239897213"],
+        "symlink": ["watch", "clock", "0426c94ea35c87780ff01dc239897213"],
+    },
+    {
+        "name": "left_ptr_watch",
+        "symlink": [
+            "progress",
+            "half_busy",
+            "half-busy",
+            "00000000000000020006000e7e9ffc3f",
+            "08e8e1c95fe2fc01f976f1e063a24ccd",
+            "3ecb610c1bf2410f44200f48c40d3599",
+            "9116a3ea924ed2162ecab71ba103b17f",
+        ],
     },
     {
         "name": "half_busy",
@@ -65,10 +41,47 @@ seed_data = [
             "9116a3ea924ed2162ecab71ba103b17f",
         ],
     },
+    # Static or Semi-Animated cursors
+    {
+        "name": "default",
+        "symlink": ["left_ptr", "top_left_arrow", "left-arrow"],
+    },
+    {
+        "name": "arrow",
+        "symlink": ["right_ptr", "top_right_arrow", "right-arrow"],
+    },
+    {"name": "center_ptr", "symlink": []},
+    {
+        "name": "link",
+        "symlink": [
+            "alias",
+            "0876e1c15ff2fc01f906f1c363074c0f",
+            "3085a0e285430894940527032f8b26df",
+            "640fb0e74195791501fd1ed57b41487f",
+            "a2a266d0498c3104214a47bd64ab0fc8",
+        ],
+    },
+    {"name": "dnd_link", "symlink": ["dnd-link"]},
+    {"name": "forbidden", "symlink": ["not-allowed"]},
+    {
+        "name": "crossed_circle",
+        "symlink": ["03b6e0fcb3499374a867c041f52298f0"],
+    },
+    {"name": "circle", "symlink": []},
+    {
+        "name": "dnd_no_drop",
+        "symlink": [
+            "dnd-no-drop",
+            "no-drop",
+            "03b6e0fcb3499374a867c041f52298f0",
+            "03b6e0fcb3499374a867d041f52298f0",
+        ],
+    },
+    {"name": "pirate", "symlink": ["kill"]},
+    {"name": "pencil", "symlink": []},
     {
         "name": "help",
         "symlink": [
-            "help",
             "question_arrow",
             "whats_this",
             "gumby",
@@ -105,7 +118,6 @@ seed_data = [
     {
         "name": "size_fdiag",
         "symlink": [
-            "size_fdiag",
             "nwse-resize",
             "38c5dff7c7b8962045400281044508d2",
             "c7088f0f3e6c8088236ef8e1e3e70000",
@@ -116,7 +128,6 @@ seed_data = [
     {
         "name": "size_bdiag",
         "symlink": [
-            "size_bdiag",
             "nesw-resize",
             "50585d75b494802d0151028115016902",
             "fcf1c3c7cd4491d801f1e1c78f100000",
@@ -126,7 +137,6 @@ seed_data = [
     {
         "name": "move",
         "symlink": [
-            "move",
             "fleur",
             "4498f0e0c1937ffe01fd06f973665830",
             "9081237383d90e509aa00f00170e968f",
@@ -137,29 +147,27 @@ seed_data = [
     {"name": "all_scroll", "symlink": ["all-scroll"]},
     {
         "name": "closedhand",
-        "symlink": ["closedhand", "grabbing", "208530c400c041818281048008011002"],
+        "symlink": ["grabbing", "208530c400c041818281048008011002"],
     },
     {"name": "dnd_none", "symlink": ["dnd-none"]},
     {
         "name": "openhand",
         "symlink": [
-            "openhand",
             "5aca4d189052212118709018842178c0",
             "9d800788f1b08800ae810202380a0822",
         ],
     },
-    {"name": "up_arrow", "symlink": ["up_arrow"]},
+    {"name": "up_arrow", "symlink": []},
     {"name": "color_picker", "symlink": ["color-picker"]},
-    {"name": "text", "symlink": ["text", "ibeam", "xterm"]},
+    {"name": "text", "symlink": ["ibeam", "xterm"]},
     {
         "name": "vertical_text",
         "symlink": ["vertical-text", "048008013003cff3c00c801001200000"],
     },
-    {"name": "crosshair", "symlink": ["crosshair"]},
+    {"name": "crosshair", "symlink": []},
     {
         "name": "copy",
         "symlink": [
-            "copy",
             "08ffe1cb5fe6fc01f906f1c063814ccf",
             "1081e37283d90000800003c07f3ef6bf",
             "6407b0e94181790501fd1e167b474872",
@@ -170,15 +178,14 @@ seed_data = [
     {
         "name": "pointer",
         "symlink": [
-            "pointer",
             "pointing_hand",
             "hand1",
             "e29285e634086352946a0e7090d73106",
         ],
     },
-    {"name": "hand2", "symlink": ["hand2"]},
-    {"name": "cross", "symlink": ["cross", "diamond_cross", "target"]},
-    {"name": "cell", "symlink": ["cell"]},
+    {"name": "hand2", "symlink": []},
+    {"name": "cross", "symlink": ["diamond_cross", "target"]},
+    {"name": "cell", "symlink": []},
     {
         "name": "col_resize",
         "symlink": [
@@ -188,7 +195,7 @@ seed_data = [
             "14fef782d02440884392942c11205230",
         ],
     },
-    {"name": "split_h", "symlink": ["split_h"]},
+    {"name": "split_h", "symlink": []},
     {
         "name": "row_resize",
         "symlink": [
@@ -198,21 +205,21 @@ seed_data = [
             "c07385c7190e701020ff7ffffd08103c",
         ],
     },
-    {"name": "split_v", "symlink": ["split_v"]},
-    {"name": "plus", "symlink": ["plus"]},
+    {"name": "split_v", "symlink": []},
+    {"name": "plus", "symlink": []},
     {"name": "X_cursor", "symlink": ["X_cursor", "X-cursor"]},
     {
         "name": "context_menu",
         "symlink": ["context-menu", "08ffe1e65f80fcfdf9fff11263e74c48"],
     },
-    {"name": "zoom", "symlink": ["zoom"]},
+    {"name": "zoom", "symlink": []},
     {
         "name": "zoom_out",
-        "symlink": ["zoom-out", "zoom_out", "f41c0e382c97c0938e07017e42800402"],
+        "symlink": ["zoom-out", "f41c0e382c97c0938e07017e42800402"],
     },
     {
         "name": "zoom_in",
-        "symlink": ["zoom-in", "zoom_in", "f41c0e382c94c0958e07017e42b00462"],
+        "symlink": ["zoom-in", "f41c0e382c94c0958e07017e42b00462"],
     },
 ]
 
@@ -303,6 +310,7 @@ class Database:
         rename_list: List[RenameCursor] = []
 
         for s in l:
+            s = path.splitext(s)[0]
             n1 = self.cursor_node_by_symlink(s)
             if n1:
                 if n1["name"] != s:
@@ -314,6 +322,7 @@ class Database:
                 if new:
                     rename_list.append(RenameCursor(old=s, new=new))
                 else:
+                    print(f"{s} is Unknown Cursor")
                     continue
 
         if rename_list:
