8cdbb7aecd017807de18a15f2ea825d372da57f3
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-02 17:46:52 +0530

    🖼️ Resize cursors .png files

--- clickgen/providers/themeconfig.py
+++ clickgen/providers/themeconfig.py
@@ -131,11 +131,13 @@ class HotspotJsonParser:
     def __init__(self, hotspots: JsonData) -> None:
         self.__hotspots = hotspots
 
-    def get_hotspot(self, c: str, old_size: ImageSize, new_size: ImageSize) -> Hotspot:
+    def get_hotspot(
+        self, key: str, old_size: ImageSize, new_size: ImageSize
+    ) -> Hotspot:
         try:
-            c = path.splitext(c)[0]
-            x = self.__hotspots[c]["xhot"]
-            y = self.__hotspots[c]["yhot"]
+            key = path.splitext(key)[0]
+            x = self.__hotspots[key]["xhot"]
+            y = self.__hotspots[key]["yhot"]
 
             xhot = int(round(new_size.width / old_size.width * x))
             yhot = int(round(new_size.height / old_size.height * y))
@@ -155,7 +157,7 @@ class HotspotJsonParser:
 class CursorConfig:
     src_png: Path = Path()
     sizes: List[ImageSize]
-    hotspot: JsonData
+    hotspot: HotspotJsonParser
     config_dir: Path = Path(tempfile.mkdtemp(prefix="clickgen_"))
 
     def __init__(
@@ -169,40 +171,38 @@ class CursorConfig:
             raise IOError(f"Invalid file format '{fp.suffix}' in {fp.name}")
 
         self.src_png = fp
-        self.png_name = self.src_png.stem
+        self.cursor = self.src_png.stem
         self.sizes = sizes
-        self.hotspot = hotspot
+        self.hotspot = HotspotJsonParser(hotspot)
 
-    # TODO
-    def __resize_cursor(self, cur: str, size: int) -> Tuple[int, int]:
+    def resize_cursor(self, new_size: ImageSize) -> Hotspot:
         """ Resize cursor .png file as @size. """
-        in_path = path.join(self.__bitmaps.bitmap_dir, cur)
-        out_dir = path.join(self.config_dir, f"{size}x{size}")
-        out_path = path.join(out_dir, cur)
-        if not path.exists(out_dir):
+        out_dir: Path = self.config_dir / f"{new_size.width}x{new_size.height}"
+        out_path: Path = out_dir / self.src_png.name
+
+        if not out_dir.exists():
             os.makedirs(out_dir)
 
         # opening original image
-        image = Image.open(in_path)
-        width: int = image.size[0]
-        height: int = image.size[1]
+        image = Image.open(self.src_png)
+        image_size: ImageSize = ImageSize(width=image.size[0], height=image.size[1])
 
-        if (width, height) != (size, size):
-            aspect: float = width / height
-            ideal_width: int = size
-            ideal_height: int = size
+        if image_size != new_size:
+            aspect: float = image_size.width / image_size.height
+            ideal_width: int = new_size.width
+            ideal_height: int = new_size.height
             ideal_aspect: float = ideal_width / float(ideal_height)
 
             if aspect > ideal_aspect:
                 # Then crop the left and right edges:
-                new_width: int = int(ideal_aspect * height)
-                offset: float = (width - new_width) / 2
-                resize = (offset, 0, width - offset, height)
+                new_width: int = int(ideal_aspect * image_size.height)
+                offset: float = (image_size.width - new_width) / 2
+                resize = (offset, 0, image_size.width - offset, image_size.height)
             else:
                 # ... crop the top and bottom:
-                new_height = int(width / ideal_aspect)
-                offset: float = (height - new_height) / 2
-                resize = (0, offset, width, height - offset)
+                new_height = int(image_size.width / ideal_aspect)
+                offset: float = (image_size.height - new_height) / 2
+                resize = (0, offset, image_size.width, image_size.height - offset)
 
             # save resized image
             thumb = image.crop(resize).resize(
@@ -213,21 +213,35 @@ class CursorConfig:
             image.close()
             thumb.close()
 
-        return self.__cords.get_hotspots(_clean_cur_name(cur), (width, height), size)
+        hotspot: Hotspot = self.hotspot.get_hotspot(self.cursor, image_size, new_size)
+        return hotspot
+
+    def write_cfg_file(self, lines: List[str]) -> None:
+        """ Write {@cur.in} file in @self.config_dir. """
+        # sort line, So all lines in order according to size (24x24, 28x28, ..)
+        lines.sort()
+
+        # remove newline from EOF
+        lines[-1] = lines[-1].rstrip("\n")
+        cfg_path = self.config_dir / f"{self.cursor}.in"
+
+        # TODO: writing with pathlib
+        with open(cfg_path, "w") as f:
+            f.writelines(lines)
 
     def generate_cursor(self, delay: Union[int, None] = None) -> List[str]:
         """ Resize cursor & return `.in` file content. """
         lines: List[str] = []
 
         for size in self.sizes:
-            (xhot, yhot) = self.__resize_cursor(self.png_name, size)
+            (xhot, yhot) = self.__resize_cursor(size)
             if delay:
                 lines.append(
-                    f"{size.width} {xhot} {yhot} {size.width}x{size.height}/{self.png_name} {delay}\n"
+                    f"{size.width} {xhot} {yhot} {size.width}x{size.height}/{self.cursor} {delay}\n"
                 )
             else:
                 lines.append(
-                    f"{size.width} {xhot} {yhot} {size.width}x{size.height}/{self.png_name}\n"
+                    f"{size.width} {xhot} {yhot} {size.width}x{size.height}/{self.cursor}\n"
                 )
 
         return lines
