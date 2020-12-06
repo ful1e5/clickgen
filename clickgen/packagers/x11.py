ac0a564c3211de0a4a18d8e1a0d442a26af28c0b
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-06 13:02:25 +0530

    📦 Crisp XPackager

--- clickgen/packagers/x11.py
+++ clickgen/packagers/x11.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from os import path
+from pathlib import Path
 from string import Template
 from typing import Dict, List
 
@@ -12,6 +13,37 @@ templates: Dict[str, Template] = {
     "cursor.theme": Template("[Icon Theme]\nInherits=$theme_name"),
     "index.theme": Template("[Icon Theme]\nName=$theme_name\nComment=$comment"),
 }
+
+
+class XPackager:
+    """ Create a crispy `XCursors` theme package. """
+
+    dir: Path = Path()
+    package_info: ThemeInfo
+
+    def __init__(self, dir: Path, info: ThemeInfo) -> None:
+        self.dir: Path = dir
+        self.package_info: ThemeInfo = info
+
+    def index_files(self) -> Dict[str, str]:
+        """ XCursors theme files. """
+        files: Dict[str, str] = {}
+        for key in templates:
+            files[key] = templates[key].safe_substitute(
+                theme_name=self.package_info.theme_name,
+                comment=self.package_info.comment,
+            )
+
+        return files
+
+    def save(self):
+        """ Make XCursor theme. """
+        # Write .theme files
+        files: Dict[str, str] = self.index_files()
+
+        for f, data in files.items():
+            fp: Path = self.dir / f
+            fp.write_text(data)
 
 
 class X11Packager:
