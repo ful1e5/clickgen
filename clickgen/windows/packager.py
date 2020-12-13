2e0ffe57d5a21d1226a44946fa6bcb831b330295
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-13 17:56:36 +0530

    🔧 Windows installation template cleanup

--- clickgen/windows/packager.py
+++ clickgen/windows/packager.py
@@ -13,34 +13,33 @@ signature="$CHICAGO$"
 $comment
 
 [DefaultInstall]
-CopyFiles = Scheme.Cur, Scheme.Txt
+CopyFiles = Scheme.Cur
 AddReg    = Scheme.Reg
 
 [DestinationDirs]
 Scheme.Cur = 10,"%CUR_DIR%"
 
 [Scheme.Reg]
-HKCU,"Control Panel\Cursors\Schemes","%SCHEME_NAME%",,"%10%\%CUR_DIR%\%pointer%,%10%\%CUR_DIR%\%help%,%10%\%CUR_DIR%\%work%,%10%\%CUR_DIR%\%busy%,%10%\%CUR_DIR%\%cross%,%10%\%CUR_DIR%\%Text%,%10%\%CUR_DIR%\%Hand%,%10%\%CUR_DIR%\%unavailiable%,%10%\%CUR_DIR%\%Vert%,%10%\%CUR_DIR%\%Horz%,%10%\%CUR_DIR%\%Dgn1%,%10%\%CUR_DIR%\%Dgn2%,%10%\%CUR_DIR%\%move%,%10%\%CUR_DIR%\%alternate%,%10%\%CUR_DIR%\%link%"
+HKCU,"Control Panel\Cursors\Schemes","%SCHEME_NAME%",,"%10%\%CUR_DIR%\%pointer%,%10%\%CUR_DIR%\%help%,%10%\%CUR_DIR%\%work%,%10%\%CUR_DIR%\%busy%,%10%\%CUR_DIR%\%Cross%,%10%\%CUR_DIR%\%Text%,%10%\%CUR_DIR%\%Hand%,%10%\%CUR_DIR%\%Unavailiable%,%10%\%CUR_DIR%\%Vert%,%10%\%CUR_DIR%\%Horz%,%10%\%CUR_DIR%\%Dgn1%,%10%\%CUR_DIR%\%Dgn2%,%10%\%CUR_DIR%\%move%,%10%\%CUR_DIR%\%alternate%,%10%\%CUR_DIR%\%link%"
 
 ; -- Installed files
 
 [Scheme.Cur]
-Work.ani
-Busy.ani
-Default.cur
-Help.cur
-Link.cur
-Move.cur
-Diagonal_2.cur
-install.inf
-Vertical.cur
-Horizontal.cur
-Diagonal_1.cur
-Handwriting.cur
-Cross.cur       
-IBeam.cur
-Unavailiable.cur
-Alternate.cur
+"Work.ani"
+"Busy.ani"
+"Default.cur"
+"Help.cur"
+"Link.cur"
+"Move.cur"
+"Diagonal_2.cur"
+"Vertical.cur"
+"Horizontal.cur"
+"Diagonal_1.cur"
+"Handwriting.cur"
+"Cross.cur"       
+"IBeam.cur"
+"Unavailiable.cur"
+"Alternate.cur"
 
 [Strings]
 CUR_DIR       = "Cursors\\$theme_name Cursors"
