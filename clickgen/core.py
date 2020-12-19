b699debecbaba58524fe134f6e9e2f299728a42f
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-19 20:19:25 +0530

    ⚡ core module init

--- clickgen/core.py
+++ clickgen/core.py
@@ -4,8 +4,8 @@
 import os
 import shutil
 import tempfile
-from pathlib import Path
-from typing import List
+from pathlib import Path, PurePath
+from typing import List, Sequence, Union
 
 from clickgen.constants import WIN_BITMAPS_SIZE
 from clickgen.providers.bitmaps import Bitmaps
@@ -123,3 +123,31 @@ def create_theme(config: Config):
     bits.remove_tmp_bitmaps()
     shutil.rmtree(xtmp)
     shutil.rmtree(wtmp)
+
+
+_P = Union[str, Path]
+_PSequence = List[_P]
+
+PurePath
+
+
+class CursorBitmap:
+    animated: bool
+    png: Path
+    cursor_key: str
+    pngs: List[Path] = []
+
+    def __init__(self, png: Union[_P, _PSequence]) -> None:
+        if isinstance(png, str) or isinstance(png, Path):
+            self.animated = False
+            self.png = Path(png)
+            self.cursor_key = self.png.stem
+        elif isinstance(png, list):
+            for p in png.sort():
+                self.pngs.append(Path(p))
+            self.animated = True
+            self.cursor_key = self.pngs[0].stem.rsplit("-", 1)
+        else:
+            raise Exception(
+                f"CursorBitmap: Unable to parse argument 'png' with Type {type(png)}"
+            )
