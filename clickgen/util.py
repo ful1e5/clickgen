7e81eff8d56d2ada6f4a187868802c3dad4d170b
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-25 17:51:08 +0530

    🔗 Debugging decorators added

--- clickgen/util.py
+++ clickgen/util.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import functools
 import os
 import shutil
 import sys
+import time
 from contextlib import contextmanager
 from difflib import SequenceMatcher as SM
 from pathlib import Path
@@ -55,3 +57,34 @@ def match_string(s: str, l: List[str]) -> Optional[str]:
         return result
     else:
         return None
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()  # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()  # 2
+        run_time = end_time - start_time  # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
+
+
+def debug(func):
+    """Print the function signature and return value"""
+
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)  # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")  # 4
+        return value
+
+    return wrapper_debug
