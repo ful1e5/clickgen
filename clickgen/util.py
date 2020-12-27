401420835f539ae9bca702691af1d6a38bbc1115
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-27 12:43:54 +0530

    ⏲ Timer fraction increased

--- clickgen/util.py
+++ clickgen/util.py
@@ -68,7 +68,7 @@ def timer(func):
         value = func(*args, **kwargs)
         end_time = time.perf_counter()  # 2
         run_time = end_time - start_time  # 3
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        print(f"Finished {func.__name__!r} in {run_time:.6f} secs")
         return value
 
     return wrapper_timer
