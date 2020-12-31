83ffda30f536455fe0375aae39b185192de8f840
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-31 20:15:12 +0530

    💝 Example Updated

--- examples/build.py
+++ examples/build.py
@@ -1,23 +1,34 @@
 import json
 from pathlib import Path
+from typing import Dict, Tuple
 
-from clickgen import core
-from clickgen.Type.core import Config, ThemeInfo, ThemeSettings, JsonData
+from clickgen.core import CursorAlias
+from clickgen.util import PNGProvider
+from clickgen.builders.x11 import XCursor
+from clickgen.packagers import XPackager
 
-with open("./hotspots.json", "r") as hotspot_file:
-    hotspots: JsonData = json.loads(hotspot_file.read())
+out_dir = Path("themes") / "xpro"
+png = PNGProvider("bitmaps")
 
-info: ThemeInfo = ThemeInfo(theme_name="BQ", author="Kaiz Khatri")
-bitmap_dir = Path("./bitmaps")
-out_dir = Path("./themes")
+SIZES = [(20, 20)]
+DELAY: int = 10
 
-sett: ThemeSettings = ThemeSettings(
-    bitmaps_dir=bitmap_dir,
-    sizes=[24, 28],
-    hotspots=hotspots,
-    animation_delay=60,
-    out_dir=out_dir,
-)
-cfg: Config = Config(info, sett)
+with open("data.json", "r") as f:
+    data: Dict[str, Dict[str, int]] = json.loads(f.read())
 
-core.create_theme(cfg)
+
+for key, data in data.items():
+    hotspot: Tuple[int, int] = (data["xhot"], data["yhot"])
+
+    try:
+        delay = data["delay"]
+    except KeyError:
+        delay = DELAY
+
+    p = png.get(key)
+
+    with CursorAlias.create_from(p, hotspot) as alias:
+        config_file = alias.alias(SIZES, delay)
+        XCursor.build_from(config_file, out_dir)
+
+    XPackager(out_dir, theme_name="xpro", comment="Xcursor pro version")
