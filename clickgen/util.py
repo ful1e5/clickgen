e5a07556124e6e5e68ef8b4c523874d43df3fcc8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-12-31 17:11:33 +0530

    🖼️ provide_pngs from directory util init

--- clickgen/util.py
+++ clickgen/util.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import functools
+import json
 import os
+import re
 import shutil
 import time
 from contextlib import contextmanager
-from pathlib import Path
-from typing import Union
+from pathlib import Path, PosixPath
+from types import SimpleNamespace
+from typing import Any, Iterable, List, Union
+
+from clickgen.core import _P, to_path
 
 
 def remove_util(p: Union[str, Path]) -> None:
@@ -47,10 +52,13 @@ def timer(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()
+
         value = func(*args, **kwargs)
+
         end_time = time.perf_counter()
         run_time = end_time - start_time
         print(f"Finished {func.__name__!r} in {run_time:.6f} secs")
+
         return value
 
     return wrapper_timer
@@ -64,9 +72,38 @@ def debug(func):
         args_repr = [repr(a) for a in args]  # 1
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
         signature = ", ".join(args_repr + kwargs_repr)  # 3
+
         print(f"Calling {func.__name__}({signature})")
         value = func(*args, **kwargs)
         print(f"{func.__name__!r} returned {value!r}")  # 4
+
         return value
 
     return wrapper_debug
+
+
+def provide_pngs(bitmaps_dir: _P, pattern: str) -> List[Union[Path, List[Path]]]:
+    bmp_dir: Path = to_path(bitmaps_dir)
+
+    animated: List[PosixPath] = []
+    static: List[PosixPath] = []
+
+    keys: Iterable[str] = []
+
+    for file in sorted(bmp_dir.glob("*.png")):
+        match = re.search(pattern, file.name)
+        if match:
+            animated.append(file)
+            key, _ = file.name.split(match.group(0))
+            keys.append(key)
+        else:
+            static.append(file)
+
+    result: List[Union[Path, List[Path]]] = static
+    keys = set(keys)
+
+    for key in keys:
+        group = list(filter(lambda f: key in f.stem, animated))
+        result.append(group)
+
+    return result
