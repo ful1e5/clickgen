8cfb55d234c348f199eb51df21d9ed53f987942a
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-01-01 18:51:15 +0530

    🧹 Cleanup example directory

--- examples/win.py
+++
@@ -1,8 +0,0 @@
-import json
-from clickgen import build_win_cursor_theme
-
-with open('./hotspots.json', 'r') as hotspot_file:
-    hotspots = json.loads(hotspot_file.read())
-
-build_win_cursor_theme(
-    name="My Cursor", image_dir="./bitmaps", cursor_sizes=[24, 28], hotspots=hotspots, out_path="./themes", delay=50)
