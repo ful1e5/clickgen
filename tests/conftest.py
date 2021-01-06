38dd309f272ae2eea66000972f429e8b7778d5eb
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-01-06 18:36:46 +0530

    📂 Init Temporary Image dir fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,35 +3,37 @@
 
 from pathlib import Path
 from random import randint
-from tempfile import mkdtemp
 from typing import List
 
 import pytest
 from PIL import Image
 
 
-def create_test_image(count: int, size=(20, 20)) -> List[Path]:
-    tmp_dir = Path(mkdtemp())
+def create_test_image(dir: Path, count: int, size=(20, 20)) -> List[Path]:
     images: List[Path] = []
-
     for c in range(count):
-        file = tmp_dir / f"test-{c}.png"
+        file = dir / f"test-{c}.png"
         Image.new(
             "RGBA",
             size=size,
             color=(randint(0, 255), randint(0, 255), randint(0, 255)),
         ).save(file, "png", compress=0)
         images.append(file)
-
     return images
 
 
+@pytest.fixture(scope="session")
+def image_dir(tmpdir_factory: pytest.TempdirFactory):
+    return Path(tmpdir_factory.mktemp("test_image"))
+
+
 @pytest.fixture(scope="module")
-def static_png():
-    p = create_test_image(1)
+def static_png(image_dir):
+    p = create_test_image(image_dir, 1)
     return p[0]
 
 
 @pytest.fixture(scope="module")
-def animated_png():
-    return create_test_image(randint(2, 5))
+def animated_png(image_dir):
+    p = create_test_image(image_dir, randint(2, 5))
+    return p
