98406efdcbf4fb947831994b970bc4ac064b81bd
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-01-06 18:37:06 +0530

    🧪 Test static bitmap key

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,54 +2,74 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+from typing import List
 
 import pytest
 from clickgen.core import Bitmap
 
 
 def test_static_Bitmap_as_str(static_png) -> None:
-    png = static_png
-    bitmap = Bitmap(str(png))
+    str_static_png = str(static_png)
+    bmp = Bitmap(str_static_png)
 
     with pytest.raises(AttributeError):
-        bitmap.grouped_png
-    assert bitmap.png == png
-    assert bitmap.animated == False
-    assert bitmap.height == 20
-    assert bitmap.width == 20
-    assert bitmap.compress == 0
-    assert bitmap.key == "test-0"
-    assert bitmap.x_hot == 0
-    assert bitmap.y_hot == 0
+        bmp.grouped_png
+    assert bmp.png == static_png
+    assert bmp.animated == False
+    assert bmp.height == 20
+    assert bmp.width == 20
+    assert bmp.compress == 0
+    assert bmp.key == "test-0"
+    assert bmp.x_hot == 0
+    assert bmp.y_hot == 0
 
 
 def test_static_Bitmap_as_Path(static_png) -> None:
-    png = static_png
-    bitmap = Bitmap(png)
+    bmp = Bitmap(static_png)
 
     with pytest.raises(AttributeError):
-        bitmap.grouped_png
-    assert bitmap.png == png
-    assert bitmap.animated == False
-    assert bitmap.height == 20
-    assert bitmap.width == 20
-    assert bitmap.compress == 0
-    assert bitmap.key == "test-0"
-    assert bitmap.x_hot == 0
-    assert bitmap.y_hot == 0
+        bmp.grouped_png
+    assert bmp.png == static_png
+    assert bmp.animated == False
+    assert bmp.height == 20
+    assert bmp.width == 20
+    assert bmp.compress == 0
+    assert bmp.key == "test-0"
+    assert bmp.x_hot == 0
+    assert bmp.y_hot == 0
 
 
-def test_animated_Bitmap_as_Path_without_key_and_hotspot(animated_png) -> None:
-    png = animated_png
-    bitmap = Bitmap(png)
+def test_animated_Bitmap_as_str(animated_png) -> None:
+    str_animated_png: List[str] = list(map(lambda x: str(x.absolute()), animated_png))
+    bmp = Bitmap(str_animated_png)
 
     with pytest.raises(AttributeError):
-        bitmap.png
-    assert bitmap.grouped_png == png
-    assert bitmap.animated == True
-    assert bitmap.height == 20
-    assert bitmap.width == 20
-    assert bitmap.compress == 0
-    assert bitmap.key == "test"
-    assert bitmap.x_hot == 0
-    assert bitmap.y_hot == 0
+        bmp.png
+    assert bmp.grouped_png == animated_png
+    assert bmp.animated == True
+    assert bmp.height == 20
+    assert bmp.width == 20
+    assert bmp.compress == 0
+    assert bmp.key == "test"
+    assert bmp.x_hot == 0
+    assert bmp.y_hot == 0
+
+
+def test_animated_Bitmap_as_Path(animated_png) -> None:
+    bmp = Bitmap(animated_png)
+
+    with pytest.raises(AttributeError):
+        bmp.png
+    assert bmp.grouped_png == animated_png
+    assert bmp.animated == True
+    assert bmp.height == 20
+    assert bmp.width == 20
+    assert bmp.compress == 0
+    assert bmp.key == "test"
+    assert bmp.x_hot == 0
+    assert bmp.y_hot == 0
+
+
+def test_key_not_affecting_in_static_Bitmap(static_png) -> None:
+    bmp = Bitmap(static_png, key="Testing")
+    bmp.key == "test-0"
