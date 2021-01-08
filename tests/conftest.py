4aa20ceffc04c0b29d4d5a4edb58d8a00e0363b8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-01-08 18:42:33 +0530

    🚀 fixtures scope updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import shutil
 from pathlib import Path
 from random import randint
 
@@ -9,30 +10,32 @@ import pytest
 from .utils import create_test_image
 
 
-@pytest.fixture(scope="session")
-def image_dir(tmpdir_factory: pytest.TempdirFactory):
-    return Path(tmpdir_factory.mktemp("test_image"))
-
-
 @pytest.fixture(scope="module")
+def image_dir(tmpdir_factory: pytest.TempdirFactory):
+    dir = Path(tmpdir_factory.mktemp("test_image"))
+    yield dir
+    shutil.rmtree(dir)
+
+
+@pytest.fixture(scope="function")
 def test_file(image_dir):
     file: Path = image_dir / "test.test"
     file.write_text("testing")
     return file
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def static_png(image_dir):
     p = create_test_image(image_dir, 1)
     return p[0]
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def animated_png(image_dir):
     p = create_test_image(image_dir, randint(2, 5))
     return p
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def hotspot():
     return (0, 0)
