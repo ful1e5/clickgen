22575d5ecd2b73d5448ddf9c04234aae00bef218
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-01-14 17:19:47 +0530

    🧪 Test CursorDB module

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import tempfile
+from pathlib import Path
+
 import pytest
 from clickgen.db import CursorDB
 
@@ -32,7 +36,7 @@ def test_CursorDB_str(data) -> None:
     db = CursorDB(data)
     assert (
         db.__str__()
-        == "CursorDB(aa=[], cc=['bb'], bb=['cc'], ffffff=['ddddd'], ddddd=['ffffff'])"
+        == "CursorDB(aa=[], bb=['cc'], cc=['bb'], ddddd=['ffffff'], ffffff=['ddddd'])"
     )
 
 
@@ -40,5 +44,64 @@ def test_CursorDB_repr(data) -> None:
     db = CursorDB(data)
     assert (
         db.__repr__()
-        == "{'aa': [], 'cc': ['bb'], 'bb': ['cc'], 'ffffff': ['ddddd'], 'ddddd': ['ffffff']}"
+        == "{'aa': [], 'bb': ['cc'], 'cc': ['bb'], 'ddddd': ['ffffff'], 'ffffff': ['ddddd']}"
     )
+
+
+def test_CursorDB_search_symlinks_without_find_similar(data) -> None:
+    db = CursorDB(data)
+    value = db.search_symlinks("a")
+    assert value is None
+
+    value = db.search_symlinks("aa")
+    assert value == []
+
+    value = db.search_symlinks("bb")
+    assert value == ["cc"]
+
+
+def test_CursorDB_search_symlinks_with_find_similar(data) -> None:
+    db = CursorDB(data)
+    value = db.search_symlinks("a", find_similar=True)
+    assert value == []
+
+    value = db.search_symlinks("fffaf", find_similar=True)
+    assert value == ["ddddd"]
+
+    value = db.search_symlinks("bb", find_similar=True)
+    assert value == ["cc"]
+
+
+def test_CursorDB_rename_file_is_not_file_exception(data) -> None:
+    db = CursorDB(data)
+    test_p = Path(tempfile.tempdir) / "fffaf"
+    with pytest.raises(FileNotFoundError) as excinfo:
+        db.rename_file(test_p)
+    assert str(excinfo.value) == f"'{test_p}' is not file"
+    test_p.unlink(missing_ok=True)
+
+    test_p_1 = Path(tempfile.mkdtemp())
+    with pytest.raises(FileNotFoundError) as excinfo:
+        db.rename_file(test_p_1)
+    assert str(excinfo.value) == f"'{test_p_1}' is not file"
+    os.rmdir(test_p_1)
+
+
+def test_CursorDB_rename_file(data) -> None:
+    db = CursorDB(data)
+
+    test_p = Path(tempfile.tempdir) / "fffaf"
+    test_p.write_text("test")
+
+    return_p = db.rename_file(test_p)
+    assert str(return_p) == str(Path(tempfile.tempdir) / "ffffff")
+
+    test_p.unlink(missing_ok=True)
+
+    test_p_1 = Path(tempfile.tempdir) / "aa"
+    test_p_1.write_text("test")
+
+    return_p = db.rename_file(test_p_1)
+    assert return_p is None
+
+    test_p_1.unlink(missing_ok=True)
