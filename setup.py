c50b0370787cd385e3cfac48060a08a6115a8671
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-03-05 17:42:24 +0530

    👷 'make' command error

--- setup.py
+++ setup.py
@@ -8,10 +8,17 @@ from shutil import which
 from setuptools import setup
 
 
+def make_path() -> str:
+    path = which("make")
+    if not path:
+        raise Exception("'make' command not found")
+    return path
+
+
 class install(_install):
     def run(self):
-        subprocess.call([which("make"), "clean", "-C", "xcursorgen"])
-        subprocess.call([which("make"), "-C", "xcursorgen"])
+        subprocess.call([make_path(), "clean", "-C", "xcursorgen"])
+        subprocess.call([make_path(), "-C", "xcursorgen"])
         _install.run(self)
 
 
