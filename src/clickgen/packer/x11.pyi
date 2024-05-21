706fbbe716fbed225414d4d1ad8613441a766ce7
Abdulkaiz Khatri <24286590+ful1e5@users.noreply.github.com>
2024-05-21 22:23:54 +0530

    chore: updated stub/type files

--- src/clickgen/packer/x11.pyi
+++ src/clickgen/packer/x11.pyi
@@ -1,7 +1,6 @@
 from pathlib import Path
 from string import Template
-from typing import Dict
 
-FILE_TEMPLATES: Dict[str, Template]
+FILE_TEMPLATES: dict[str, Template]
 
 def pack_x11(dir: Path, theme_name: str, comment: str) -> None: ...
--- src/clickgen/parser/__init__.pyi
+++ src/clickgen/parser/__init__.pyi
@@ -1,7 +1,6 @@
 from clickgen.parser.base import BaseParser
 from clickgen.parser.png import MultiPNGParser as MultiPNGParser, SinglePNGParser as SinglePNGParser
-from typing import List, Tuple
 
 __all__ = ['SinglePNGParser', 'MultiPNGParser', 'open_blob']
 
-def open_blob(blob: bytes | List[bytes], hotspot: Tuple[int, int], sizes: List[int] | None = None, delay: int | None = None) -> BaseParser: ...
+def open_blob(blob: bytes | list[bytes], hotspot: tuple[int, int], sizes: list[int] | None = None, delay: int | None = None) -> BaseParser: ...
